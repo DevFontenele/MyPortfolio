ef6424738d32b5ffc0754938e03bccaa43d1340a
Ryan <dev.fonte@gmail.com>
2025-12-14 17:53:40 -0300

    Finalizacão das primeiras abas

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-// Copyright 2025 rfafd
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     https://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+const resumeLists = document.querySelectorAll(".resume-list");
+const resumeBoxs = document.querySelectorAll(".resume-box");
 
+const portfolioLists = document.querySelectorAll(".portfolio-list");
+const portfolioBoxs = document.querySelectorAll(".portfolio-box");
+
+
+
+
+resumeLists.forEach((list, idx) => {
+  list.addEventListener("click", () => {
+    document.querySelector(".resume-list.active").classList.remove("active");
+    list.classList.add('active')
+
+    document.querySelector(".resume-box.active").classList.remove("active");
+    resumeBoxs[idx].classList.add('active')
+  });
+});
+
+
+portfolioLists.forEach((list, idx) => {
+  list.addEventListener("click", () => {
+    document.querySelector(".portfolio-list.active").classList.remove("active");
+    list.classList.add('active')
+
+     document.querySelector(".portfolio-box.active").classList.remove("active");
+    portfolioBoxs[idx].classList.add('active')
+ 
+  });
+});
